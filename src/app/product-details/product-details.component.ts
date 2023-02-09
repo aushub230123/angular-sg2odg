1debc18b078c69fbb7cb31dfac1612ef1ebea95e
aushub230123 <123364834+aushub230123@users.noreply.github.com>
2023-02-09 11:50:55 +0530

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../child/child1/productsFileModule';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
-
+export class ProductDetailsComponent implements OnInit {
+  product : Product | undefined;
+ constructor(private route : ActivatedRoute){
+// CALLED FIRST 
+console.log("cons");
+ }
+//  LIFE-CYCLE HOOK
+ ngOnInit() {
+  //  CALLED NEXT
+  console.log("ngOnInit")
+  const routeParams = this.route.snapshot.paramMap;
+  const productIdFromRoute = Number(routeParams.get('productId'));
+  this.product = products.find(product => product.id === productIdFromRoute );
 }
+}
+
+
